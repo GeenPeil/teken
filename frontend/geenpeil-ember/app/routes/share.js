5bc7a319945d2386979c3d34e9516234e4f86c0d
Michiel van den Anker <michiel.van.den.anker@gmail.com>
2015-08-06 08:41:37 +0200

    Updated text placeholders.

--- frontend/geenpeil-ember/app/routes/share.js
+++ frontend/geenpeil-ember/app/routes/share.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   //TODO handle social buttons
-  //TODO handle back button
+
+  actions : {
+
+    back : function() {
+      this.transitionTo('home');
+    }
+
+  }
 
 });
