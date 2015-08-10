bb25bca27ee59014412b6d8cb48ddb2374563ee6
GeertJohan <gjr19912@gmail.com>
2015-08-10 14:53:50 +0200

    Switch to gogo/protobuf's protoc-gen-gogofaster for faster encoding/decoding of proto messages

--- data/doc.go
+++ data/doc.go
@@ -1,7 +1,7 @@
 // Package data contains the Handtekening struct which can be unmarshalled from json (http api) and marshalled with proto (storage).
 package data
 
-//go:generate protoc --go_out=. handtekening.proto
+//go:generate protoc --gogofaster_out=. handtekening.proto
 //go:generate ffjson -noencoder handtekening.pb.go
 
-//go:generate protoc --go_out=. gph.proto
+//go:generate protoc --gogofaster_out=. gph.proto
