a42b07ca0f55cd45aebb46b69a90871dbe4b2dd9
GeertJohan <gjr19912@gmail.com>
2015-08-11 16:16:13 +0200

    Add juncker initial setup (flags, pdf type and methods outline, basic adding of text/img to pdf file)

---
+++ juncker/options.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	goflags "github.com/jessevdk/go-flags"
+)
+
+var flags struct {
+	Verbose bool `long:"verbose" short:"v" description:"Show verbose debug information"`
+
+	Single    *uint64 `short:"s" long:"single" description:"Single handtekening to render"`
+	Partition *uint64 `short:"p" long:"partitie" description:"Partition of handtekeningen to render"`
+
+	StoragePrivkeyFile string `long:"storage-pubkey-file" description:"storage public key" default:"../storage/testpub.pem"` // TODO: remove default, make mandatory
+	StorageLocation    string `long:"storage-location" desciption:"storage location" default:"../storage/testdata"`          // TODO: remove default, make mandatory
+}
+
+func parseFlags() {
+	parser := goflags.NewParser(&flags, goflags.HelpFlag|goflags.PrintErrors)
+
+	// parse flags
+	args, err := parser.Parse()
+	if err != nil {
+		// assert the err to be a flags.Error
+		flagError, ok := err.(*goflags.Error)
+		if !ok {
+			// not a flags error
+			os.Exit(1)
+		}
+		if flagError.Type == goflags.ErrHelp {
+			// exitcode 0 when user asked for help
+			fmt.Println("Read the README.md for more usage information.")
+			os.Exit(0)
+		}
+		// error on left-over arguments
+		if len(args) > 0 {
+			fmt.Printf("unexpected arguments: %s\n", args)
+			os.Exit(0)
+		}
+		// error on left-over arguments
+		if len(args) > 0 {
+			fmt.Printf("unexpected arguments: %s\n", args)
+			os.Exit(0)
+		}
+		if flagError.Type == goflags.ErrUnknownFlag {
+			fmt.Println("run with --help to view available options")
+		}
+		os.Exit(1)
+	}
+
+	// error on left-over arguments
+	if len(args) > 0 {
+		fmt.Printf("unexpected arguments: %s\n", args)
+		os.Exit(0)
+	}
+
+	// required flags
+	if flags.Single == nil && flags.Partition == nil {
+		fmt.Println("Require either --single or --partition flag to render a single or partition of handtekeningen to pdf.")
+		os.Exit(42)
+	}
+}
---
+++ juncker/pdf.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/jung-kurt/gofpdf"
+
+	"github.com/GeenPeil/teken/data"
+	"github.com/GeertJohan/go.incremental"
+)
+
+var pngtp string
+
+func init() {
+	p := gofpdf.New("P", "mm", "A4", "")
+	pngtp = p.ImageTypeFromMime("image/png")
+}
+
+type pdf struct {
+	name string
+	fpdf *gofpdf.Fpdf
+	inc  incremental.Uint64
+}
+
+func NewPDF(name string) *pdf {
+	return &pdf{
+		name: name,
+		fpdf: gofpdf.New("P", "mm", "A4", ""),
+	}
+}
+
+func (p *pdf) AddHandtekening(h *data.Handtekening) error {
+	p.fpdf.AddPage()
+	p.fpdf.SetFont("Arial", "", 11)
+	// p.fpdf.Image(imageFile("logo.png"), 10, 10, 30, 0, false, "", 0, "")
+	p.fpdf.Text(50, 20, h.Voornaam)
+	p.fpdf.Text(50, 30, h.Tussenvoegsel)
+	p.fpdf.Text(50, 40, h.Achternaam)
+	p.fpdf.Text(50, 50, h.Geboortedatum)
+	p.fpdf.Text(50, 60, h.Geboorteplaats)
+	p.fpdf.Text(50, 70, h.Straat)
+	p.fpdf.Text(50, 80, h.Huisnummer)
+	p.fpdf.Text(50, 90, h.Postcode)
+	p.fpdf.Text(50, 100, h.Woonplaats)
+	handtekeningReader := base64.NewDecoder(base64.StdEncoding, bytes.NewBuffer(h.Handtekening))
+	imgName := fmt.Sprintf("h-%d", p.inc.Next())
+	p.fpdf.RegisterImageReader(imgName, pngtp, handtekeningReader)
+	p.fpdf.Image(imgName, 50, 110, 0, 0, false, pngtp, 0, "")
+
+	return nil
+}
+
+func (p *pdf) Render(filename string) error {
+	return p.fpdf.OutputFileAndClose(filename)
+}
