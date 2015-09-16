08e54f63e5898ff254aea9e2cf2626445df7cf8d
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-09-16 21:09:59 +0200

    Add social buttons to thanks page.

--- pechtold/server/verify.go
+++ pechtold/server/verify.go
@@ -15,6 +15,15 @@ var (
 <html>
 	<head>
 		<title>GeenPeil verificatie</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+		<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+		<script>
+		function shareLink(element) {
+			var href = element.getAttribute('href');
+			var newWindow = window.open(href, 'newwindow', 'left=300, top=200, width=600, height=475');
+			return !newWindow;
+		}
+		</script>
 		<style>
 			body {
 				background-color: #04339a;
@@ -33,6 +42,39 @@ var (
 				margin-bottom: 35px;
 				vertical-align: middle;
 			}
+			.geenpeil-share {
+				background-color: white;
+				display: inline-block;
+				padding: 7px;
+				font-family: 'Montserrat', 'Arial', sans-serif;
+				box-shadow: 1px 1px 1px 1px rgba(80,80,80,0.6);
+				border-radius: 4px;
+				text-align: center;
+			}
+			.geenpeil-share .share-button {
+				transition: all 0.3s ease;
+				font-size: 40px;
+				margin: 4px;
+				text-decoration: none;
+				text-shadow: 2px 2px 2px #ccc;
+			}
+			.geenpeil-share .share-title {
+				color: black;
+				display: inline-block;
+				margin: 5px;
+			}
+			.geenpeil-share .share-button:hover {
+				text-shadow: 3px 3px 5px #bbb;
+			}
+			.geenpeil-share .share-button.facebook {
+				color: #395693;
+			}
+			.geenpeil-share .share-button.twitter {
+				color: #39a9e0;
+			}
+			.geenpeil-share .share-button.googleplus {
+				color: #d14836;
+			}
 		</style>
 	</head>
 	<body>
@@ -42,6 +84,14 @@ var (
 			{{if .Verdrietig}}
 				<img src="/pechtold-static/verdrietig.jpg" />
 			{{end}}
+			<br/>
+			<div class="geenpeil-share">
+				<span class="share-title">Help en <b>DEEL GEENPEIL</b> met iedereen!</span>
+				<br/>
+				<a title="Deel op Facebook" class="share-button facebook fa fa-facebook-official" href="https://www.facebook.com/sharer/sharer.php?u=teken.geenpeil.nl" onclick="return shareLink(this);"></a>
+				<a title="Deel op Twitter" class="share-button twitter fa fa-twitter-square" href="https://twitter.com/home?status=Ik heb getekend! Red de democratie en teken ook op https%3A%2F%2Fteken.geenpeil.nl %23geenpeil" onclick="return shareLink(this);"></a>
+				<a title="Deel op Google+" class="share-button googleplus fa fa-google-plus-square" href="https://plus.google.com/share?url=www.geenpeil.nl" onclick="return shareLink(this);"></a>
+				</div>
 		</center>
 	</body>
 </html>
