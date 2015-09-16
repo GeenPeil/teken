b66c7e2b36bf7da82c216d5242a3a09b300ed991
Geert-Johan Riemer <geertjohan@geertjohan.net>
2015-09-16 22:43:17 +0200

    Add maillist tool

---
+++ maillist/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+
+	"github.com/GeenPeil/teken/storage"
+)
+
+func main() {
+	parseFlags()
+
+	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+
+	fetcher, err := storage.NewFetcher(flags.StoragePrivkeyFile, flags.StorageLocation)
+	if err != nil {
+		log.Fatalf("error creating new fetcher: %v\n", err)
+		return
+	}
+
+	chIDs := make(chan uint64)
+	go func() {
+		if flags.Single != nil {
+			chIDs <- *flags.Single
+			close(chIDs)
+		} else if flags.Partition != nil {
+			handtekeningenIDList, err := fetcher.ListPartition(*flags.Partition)
+			if err != nil {
+				log.Fatalf("error listing handtekeningen for partition %d: %v", *flags.Partition, err)
+			}
+			for _, id := range handtekeningenIDList {
+				chIDs <- id
+			}
+			close(chIDs)
+		} else if flags.All {
+			for i := uint64(1); true; i++ {
+				fmt.Printf("walking partition %04d\n", i)
+				handtekeningenIDList, err := fetcher.ListPartition(i)
+				if err != nil {
+					log.Printf("error listing handtekeningen for partition %d: %v", i, err)
+					break
+				}
+				for _, id := range handtekeningenIDList {
+					chIDs <- id
+				}
+			}
+			close(chIDs)
+		}
+	}()
+
+	var spreadsheetLock sync.Mutex
+	spreadsheetFile, err := os.Create(flags.CSVFile)
+	if err != nil {
+		log.Fatalf("error opening csv file: %v", err)
+	}
+	spreadsheet := csv.NewWriter(spreadsheetFile)
+	spreadsheet.Write([]string{"ID", "Voornaam", "Tussenvoegsel", "Achternaam", "Email"})
+
+	var numWorkers = (runtime.NumCPU() - 1) * 2
+	wgDone := sync.WaitGroup{}
+	wgDone.Add(numWorkers)
+	for n := 0; n < numWorkers; n++ {
+		go func(n int) {
+			for {
+				id, ok := <-chIDs
+				if !ok {
+					break
+				}
+				h, err := fetcher.Fetch(id)
+				if err != nil {
+					log.Printf("error fetching %06d\n", id)
+					continue
+				}
+				fields := []string{strconv.FormatUint(id, 10), h.Voornaam, h.Tussenvoegsel, h.Achternaam, h.Email}
+				spreadsheetLock.Lock()
+				err = spreadsheet.Write(fields)
+				if err != nil {
+					log.Printf("error writing fields for %06d: %v\n", id, err)
+				}
+				spreadsheetLock.Unlock()
+			}
+
+			fmt.Printf("worker %02d done\n", n)
+			wgDone.Done()
+		}(n)
+	}
+	wgDone.Wait()
+
+	spreadsheet.Flush()
+	err = spreadsheetFile.Close()
+	if err != nil {
+		log.Printf("error closing csv file: %v", err)
+	}
+}
---
+++ maillist/options.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	goflags "github.com/jessevdk/go-flags"
+)
+
+var flags struct {
+	Verbose bool `long:"verbose" short:"v" description:"Show verbose debug information"`
+
+	Single    *uint64 `short:"s" long:"single" description:"Single handtekening to render"`
+	Partition *uint64 `short:"p" long:"partitie" description:"Partition of handtekeningen to render"`
+	All       bool    `short:"a" long:"all" description:"Get all mail addresses"`
+
+	StoragePrivkeyFile string `long:"storage-privkey-file" description:"storage private key" default:"../storage/testpriv.pem"` // TODO: remove default, make mandatory
+	StorageLocation    string `long:"storage-location" desciption:"storage location" default:"../storage/testdata"`             // TODO: remove default, make mandatory
+
+	CSVFile string `long:"csv-file" description:"csv file location" default:"output.csv"` // TODO: remove default, make mandatory
+}
+
+func parseFlags() {
+	parser := goflags.NewParser(&flags, goflags.HelpFlag|goflags.PrintErrors)
+
+	// parse flags
+	args, err := parser.Parse()
+	if err != nil {
+		// assert the err to be a flags.Error
+		flagError, ok := err.(*goflags.Error)
+		if !ok {
+			// not a flags error
+			os.Exit(1)
+		}
+		if flagError.Type == goflags.ErrHelp {
+			// exitcode 0 when user asked for help
+			fmt.Println("Read the README.md for more usage information.")
+			os.Exit(0)
+		}
+		// error on left-over arguments
+		if len(args) > 0 {
+			fmt.Printf("unexpected arguments: %s\n", args)
+			os.Exit(0)
+		}
+		// error on left-over arguments
+		if len(args) > 0 {
+			fmt.Printf("unexpected arguments: %s\n", args)
+			os.Exit(0)
+		}
+		if flagError.Type == goflags.ErrUnknownFlag {
+			fmt.Println("run with --help to view available options")
+		}
+		os.Exit(1)
+	}
+
+	// error on left-over arguments
+	if len(args) > 0 {
+		fmt.Printf("unexpected arguments: %s\n", args)
+		os.Exit(0)
+	}
+
+	// required flags
+	if !flags.All && flags.Single == nil && flags.Partition == nil {
+		fmt.Println("Require either --single, --partition or --all flag to render a single or partition of handtekeningen to pdf.")
+		os.Exit(42)
+	}
+}
