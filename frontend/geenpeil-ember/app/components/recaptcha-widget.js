104550837a13e16e7a722e3a21c02e27b821fdad
Michiel van den Anker <michiel.van.den.anker@gmail.com>
2018-05-06 20:58:56 +0200

    Changed loading of reCaptcha to be lazy.

--- frontend/geenpeil-ember/app/components/recaptcha-widget.js
+++ frontend/geenpeil-ember/app/components/recaptcha-widget.js
@@ -8,17 +8,31 @@ export default Component.extend({
   }),
 
   didInsertElement : function() {
+    if(!window.grecaptcha) {
+      var script = document.createElement('script');
+      window.onRecaptchaLoaded = function() {
+        this.renderCaptcha();
+      }.bind(this);
+      script.src = 'https://www.google.com/recaptcha/api.js?onload=onRecaptchaLoaded&render=explicit&hl=nl';
+      document.getElementsByTagName('head')[0].appendChild(script);
+    }
+    else {
+      this.renderCaptcha();
+    }
+  },
+
+  renderCaptcha : function() {   
     window.grecaptcha.render(this.get('recaptchaElementId'), {
-        'sitekey' : this.get('formItem.sitekey'),
-        'callback' : function(v) {
-          this.set('formItem.value',v);
-          this.set('formItem.isValid',true);
-        }.bind(this),
-        'expired-callback' : function() {
-          this.set('formItem.value', undefined);
-          this.set('formItem.isValid', undefined);
-        }.bind(this),
-        'theme' : this.get('formItem.theme')
+      'sitekey' : this.get('formItem.sitekey'),
+      'callback' : function(v) {
+        this.set('formItem.value',v);
+        this.set('formItem.isValid',true);
+      }.bind(this),
+      'expired-callback' : function() {
+        this.set('formItem.value', undefined);
+        this.set('formItem.isValid', undefined);
+      }.bind(this),
+      'theme' : this.get('formItem.theme')
     });
   }
 
